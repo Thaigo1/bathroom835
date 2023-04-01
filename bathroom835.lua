237231d5a727a7a034edc50cda09efd06f9d637b
Thaigo1 <119961030+Thaigo1@users.noreply.github.com>
2023-03-31 21:59:51 -0500

    Update bathroom835.lua

--- bathroom835.lua
+++ bathroom835.lua
@@ -79,6 +79,13 @@ PlayerHub2:AddButton({
   	end    
 })
 
+PlayerHub2:AddButton({
+	Name = "Simple Spy",
+	Callback = function()
+        loadstring(game:HttpGet('https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua'))()
+  	end    
+})
+
 local Section = PlayerHub2:AddSection({
 	Name = "Animation Script"
 })
@@ -124,4 +131,202 @@ PlayerHub3:AddButton({
         game:GetService("ReplicatedStorage").EntityInfo.PlayAgain:FireServer()
   	end    
 })
+
+local PlayerHub4 = Window:MakeTab({
+	Name = "Break In",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+ 
+local Section = PlayerHub4:AddSection({
+	Name = "Give Roles"
+})
+
+PlayerHub4:AddButton({
+	Name = "Swat Role",
+	Callback = function()
+		local A_1 = "SwatGun"
+		local A_2 = true
+		local Event = game:GetService("ReplicatedStorage").RemoteEvents.OutsideRole
+		Event:FireServer(A_1, A_2)
+  	end    
+})
+
+local Section = PlayerHub4:AddSection({
+	Name = "Give Guns (Execute it while in a round of the game)"
+})
+
+PlayerHub4:AddButton({
+	Name = "Swat Gun (PATCH)",
+	Callback = function()
+		local args = {
+			[1] = true,
+			[2] = "Swat Gun"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.BasementWeapon:FireServer(unpack(args))
+	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Police (PATCH)",
+	Callback = function()
+		local args = {
+			[1] = true,
+			[2] = "Police Gun"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.BasementWeapon:FireServer(unpack(args))
+	end    
+})
+
+
+local Section = PlayerHub4:AddSection({
+	Name = "Give Item"
+})
+
+PlayerHub4:AddButton({
+	Name = "Key",
+	Callback = function()
+        local args = {
+			[1] = "Key"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))		
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "MedKit",
+	Callback = function()
+        local args = {
+			[1] = "MedKit"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))		
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Teddy",
+	Callback = function()
+        local args = {
+			[1] = "TeddyBloxpin"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))		
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Chips",
+	Callback = function()
+        local args = {
+			[1] = "Chips"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))		
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "BloxyCola",
+	Callback = function()
+        local args = {
+			[1] = "BloxyCola"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))		
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Apple",
+	Callback = function()
+        local args = {
+			[1] = "Apple"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))		
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Cookie",
+	Callback = function()
+local args = {
+    [1] = "Cookie"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))
+	
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Pizza",
+	Callback = function()
+local args = {
+    [1] = "Pizza3"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.GiveTool:FireServer(unpack(args))
+	
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Bat",
+	Callback = function()
+		local args = {
+			[1] = true,
+			[2] = "Bat"
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.BasementWeapon:FireServer(unpack(args))			
+  	end    
+})
+
+local Section = PlayerHub4:AddSection({
+	Name = "Extra"
+})
+
+PlayerHub4:AddButton({
+	Name = "Give Ladder (Try to be below the attic)",
+	Callback = function()
+		local args = {
+			[1] = 1
+		}
+		
+		game:GetService("ReplicatedStorage").RemoteEvents.Ladder:FireServer(unpack(args))
+  	end    
+})
+
+PlayerHub4:AddButton({
+	Name = "Unlock Door",
+	Callback = function()
+		game:GetService("ReplicatedStorage").RemoteEvents.UnlockDoor:FireServer()
+  	end    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
